4c2e1b6a11d9baf883931510e80e753188d009e4
Jacob Brown <brown.jacob.alexander@gmail.com>
2026-09-25 10:27:55 -0600

    update Person to handle name changes

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -3,6 +3,37 @@ import './App.css';
 
 const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple'];
 
+function Person() {
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Doe");
+  const [age, setAge] = useState(100);
+
+  const fullName = firstName + " " + lastName;
+
+  const handleIncreaseAge = () => {
+    setPerson(age + 1);
+  };
+
+  function handleFirstNameChange(e){
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e){
+    setLastName(e.target.value);
+  }
+
+  return (
+    <>
+      <h1>{fullName}</h1>
+      <h2>{age}</h2>
+      <button onClick={handleIncreaseAge}>Increase age</button>
+      <label for="firstNameInput">First name: </label> <input id="firstNameInput" onChange={handleFirstNameChange} value={firstName} />
+      <label for="lastNameInput">Last name: </label> <input id="lastNameInput" onChange={handleLastNameChange} value={lastName}/>
+    </>
+  );
+}
+
+
 function App() {
   const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
   const [numChanges, setNumChanges] = useState(0);
@@ -35,4 +66,4 @@ function App() {
   );
 }
 
-export default App;
+export { App, Person };
